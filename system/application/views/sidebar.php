8b47e0514569b8183e2a1e4b99366fe9e7ea1fe7
mengu <mengukagan@gmail.com>
2009-11-06 10:06:49 +0200

    Added sidebar

---
+++ system/application/views/sidebar.php
@@ -0,0 +1,24 @@
+<div id="sidebar">
+	
+	<div class="boxtitle" style="margin: 0;">Search The Blog</div>
+	<div class="boxcontent">
+	<form method="post" action="post/search">
+	<input type="text" name="query" /> <input type="submit" value="Search" />
+	</form>
+	</div>
+	
+	<div class="boxtitle">Recent Posts</div>
+	<div class="boxcontent">
+	<? foreach ($recentPosts AS $recentPost): ?>
+		<div><a href="<?=base_url();?>posts/view/<?=$recentPost->id;?>"><?=$recentPost->title;?></a></div>
+	<? endforeach; ?>
+	</div>
+	
+	<div class="boxtitle">Recent Comments</div>
+	<div class="boxcontent">
+	<? foreach ($recentComments AS $recentComment): ?>
+		<div>On <a href="<?=base_url();?>posts/view/<?=$recentComment->postid;?>"><?=$recentComment->title;?></a> by <?=$recentComment->name;?></div>
+	<? endforeach; ?>
+	</div>
+	
+  </div>
