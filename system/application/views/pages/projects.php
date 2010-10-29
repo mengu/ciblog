1fa79ed6633f4dd46bafc60a13fc808c9a78204f
mengu <mengukagan@gmail.com>
2010-10-30 02:01:37 +0300

    updated the projects page.

--- system/application/views/pages/projects.php
+++ system/application/views/pages/projects.php
@@ -5,7 +5,30 @@
         <div class="unit size2of3">
         
              <div class="page">
-                Preparing the projects page.
+                <p>
+                    <h2>CIBlog</h2>
+                    <div><a href="http://github.com/mengu/ciblog">CIBlog</a> is the blog application which powers this web site. As you might guess from
+                    its name, this app is built with CodeIgniter and updated whenever it's needed.</div>
+               </p>
+                
+                <p>
+                	<h2>IdeaBox</h2>
+                	<div><a href="http//github.com/mengu/ideabox">IdeaBox</a> is a web application that I have been building with Pylons web framework. It is 
+                	actively under development. Currently, I and Chris Moylan are calloborating on the project.</div>
+               	</p>
+               	
+               	<p>
+               		<h2>Expensii</h2>
+               		<div><a href="http://github.com/mengu/expensii">Expensii</a> is an expenses web application that I have built with the Yii framework. I'm updating 
+               		the Expensii app whenever it's needed.</div>
+               	</p>
+                               
+               <p>
+                   <div>All of these applications are open source and licensed under GPL. Please visit
+                   <a href="http://github.com/mengu">my github repository</a> for the rest of my applications. Private applications are not 
+                   listed here.</div>                                       
+               	
+                
               </div>
         </div>
 <?= $sidebar; ?>
